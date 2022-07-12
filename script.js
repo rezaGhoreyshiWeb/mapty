6fab72d93f5b84a0f4e2c7009aee9f6466b50968
rezaGhoreyshiWeb <rezaghoreyshi.contact@gmail.com>
2022-07-12 16:37:10 +0430

    change lang, lat , map to let in global scope

--- script.js
+++ script.js
@@ -10,18 +10,25 @@ const inputDistance = document.querySelector(".form__input--distance");
 const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
-console.log(L);
+
+// variables
+let longitude, latitude;
+let map;
 
 // getting the location of the user
 if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(successCallGeolocation, errorCallGeolocation);
+  navigator.geolocation.getCurrentPosition(
+    successCallGeolocation,
+    errorCallGeolocation
+  );
 }
 
 // function will be called when the geolocation is available
 function successCallGeolocation(position) {
-  const { longitude, latitude } = position.coords;
-  const coords = [latitude , longitude ];
-  const map = L.map("map").setView(coords,  13 );
+  latitude = position.coords.latitude;
+  longitude = position.coords.longitude;
+  const coords = [latitude, longitude];
+  map = L.map("map").setView(coords, 13);
 
   L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
     attribution:
@@ -34,14 +41,17 @@ function successCallGeolocation(position) {
     .openPopup();
 }
 // function will be called when the geolocation is not available
-function errorCallGeolocation(){
-    const coords = [ 35.68, 51.36];
-    const map = L.map("map").setView(coords,  10);
+function errorCallGeolocation() {
+  const coords = [35.68, 51.36];
+  map = L.map("map").setView(coords, 10);
 
-    L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
-      attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(map);
+  L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
 
-    L.marker(coords).addTo(map).bindPopup(" 🗺️ Your Current Location Not Available<br> Fine it Manually.").openPopup();
+  L.marker(coords)
+    .addTo(map)
+    .bindPopup(" 🗺️ Your Current Location Not Available<br> Fine it Manually.")
+    .openPopup();
 }
